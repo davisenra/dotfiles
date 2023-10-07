76b365732b73e9463fe51ecbd42576f3b50f8622
Davi Senra <davi.senra98@gmail.com>
2023-10-07 17:52:12 -0300

    Fix: force symlink if the files exists

--- symlinks.sh
+++ symlinks.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env sh
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/kitty/kitty.conf ~/.config/kitty
+
+source_zshrc=~/dotfiles/.zshrc
+target_zshrc=~/.zshrc
+
+source_kitty_conf=~/dotfiles/kitty/kitty.conf
+target_kitty_conf=~/.config/kitty/kitty.conf
+
+create_link() {
+    if [ -e "$2" ]; then
+        rm "$2"
+        echo "Removed existing file: $2"
+    fi
+    ln -s "$1" "$2"
+    echo "Created symlink: $2 -> $1"
+}
+
+create_link "$source_zshrc" "$target_zshrc"
+create_link "$source_kitty_conf" "$target_kitty_conf"
