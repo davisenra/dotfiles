073227082b59325f2de263aabea547567bbb4db6
Davi Senra <davi.senra98@gmail.com>
2024-05-17 21:33:21 -0300

    new .zshrc

--- symlinks.sh
+++ symlinks.sh
@@ -6,6 +6,9 @@ target_zshrc=~/.zshrc
 source_kitty_conf=~/dotfiles/kitty/kitty.conf
 target_kitty_conf=~/.config/kitty/kitty.conf
 
+source_fastfetch_conf=~/dotfiles/fastfetch/config.jsonc
+target_fastfetch_conf=~/.config/fastfetch/config.jsonc
+
 create_link() {
     if [ -e "$2" ]; then
         rm "$2"
@@ -17,3 +20,4 @@ create_link() {
 
 create_link "$source_zshrc" "$target_zshrc"
 create_link "$source_kitty_conf" "$target_kitty_conf"
+create_link "$source_fastfetch_conf" "$target_fastfetch_conf"
