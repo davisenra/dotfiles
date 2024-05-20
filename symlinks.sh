f784271928d6bd4eb3049f21b1cc20d7f77f97e7
Davi Senra <davi.senra98@gmail.com>
2024-05-20 20:29:39 -0300

    adds tmux and alacritty configs

--- symlinks.sh
+++ symlinks.sh
@@ -9,6 +9,12 @@ target_kitty_conf=~/.config/kitty/kitty.conf
 source_fastfetch_conf=~/dotfiles/fastfetch/config.jsonc
 target_fastfetch_conf=~/.config/fastfetch/config.jsonc
 
+source_alacritty_conf=~/dotfiles/alacritty/.alacritty.toml
+target_alacritty_conf=~/.alacritty.toml
+
+source_tmux_conf=~/dotfiles/tmux/.tmux.conf
+target_tmux_conf=~/.tmux.conf
+
 create_link() {
     if [ -e "$2" ]; then
         rm "$2"
@@ -21,3 +27,5 @@ create_link() {
 create_link "$source_zshrc" "$target_zshrc"
 create_link "$source_kitty_conf" "$target_kitty_conf"
 create_link "$source_fastfetch_conf" "$target_fastfetch_conf"
+create_link "$source_alacritty_conf" "$target_alacritty_conf"
+create_link "$source_tmux_conf" "$target_tmux_conf"
